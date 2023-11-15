cda4a2b621715af3d8c7e495015dc4d43508767e
MichaelSizemore2024 <u1311515@utah.edu>
2023-11-15 16:43:34 -0700

    Fixed set up for connecting to scylladb

--- db-manager/connect.cpp
+++ db-manager/connect.cpp
@@ -3,6 +3,8 @@
 #include <iostream>
 #include <memory>
 
+using namespace std;
+
 int main(int argc, char* argv[]) {
 
   // Allocate the objects that represent cluster and session. 
@@ -12,14 +14,20 @@ int main(int argc, char* argv[]) {
   // Add the contact points.
   cass_cluster_set_contact_points(cluster, "scylla");
 
+  // Tries to connect, will block until connect or refused
   auto connect_future = std::unique_ptr<CassFuture, decltype(&cass_future_free)>(cass_session_connect(session, cluster), &cass_future_free);
   if (cass_future_error_code(connect_future.get()) == CASS_OK) {
-    cout << "Connected" << std::endl;
+    cout << "Connected" << endl;
   }
   else{
-
+    //Prints out error code if unable to connect
+    cout << "Connection Error: " << cass_error_desc(cass_future_error_code(connect_future.get())) << endl;
+    cass_cluster_free(cluster);
+    cass_session_free(session);
+    return 1;
   }
 
+  // Releases the allocated resources
   cass_cluster_free(cluster);
   cass_session_free(session);
 }
