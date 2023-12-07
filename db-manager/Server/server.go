65ed5289802e17d4b3eb2ed581628d76ea9ecc7b
Jon Thompson <u1120954@utah.edu>
2023-12-07 21:38:51 +0000

    Replace server.go

--- db-manager/Server/server.go
+++ db-manager/Server/server.go
@@ -7,16 +7,18 @@ import (
 	"log"
 	"net"
 	"os"
-	//"reflect"
+
+	// "reflect"
+	"strconv"
 	"sync"
 
-	"github.com/gocql/gocql" // for scylla
+	"github.com/gocql/gocql" // For scylla
 
 	Routes "dbmanager/common" // Import the generated code
 
 	"google.golang.org/grpc"
-	"google.golang.org/grpc/reflection"
-	//"google.golang.org/protobuf/types/known/anypb" // import ANY class for future use
+	// "google.golang.org/grpc/reflection"
+	// "google.golang.org/protobuf/types/known/anypb" // import ANY class for future use
 )
 
 // variabels that can be passed in via command line such as --port=50051 or -port=50051
@@ -101,11 +103,29 @@ func (server *server) deleteKeyspace(keyspaceName string) error {
 }
 
 // GetData implements the GetData RPC method
-func (s *server) InsertEmail(ctx context.Context, req *Routes.EmailData) (*Routes.EmailResponse, error) {
+func (s *server) InsertEmail(ctx context.Context, request *Routes.EmailData) (*Routes.EmailResponse, error) {
 	s.mu.Lock()
-	defer s.mu.Unlock() // defer runs this once returned from this method
+	defer s.mu.Unlock()
 
-	return &Routes.EmailResponse{Message: "Received Email"}, nil
+	// Create a session to interact with the database
+	session, err := s.cluster.CreateSession()
+	if err != nil {
+		return nil, err
+	}
+	defer session.Close()
+
+	// Insert email data into the database
+	emailInsertQuery := fmt.Sprintf(`
+		INSERT INTO %s.%s (label, text)
+		VALUES (?, ?)
+	`, "testkeyspacename", "email_data") // Keyspace and table name
+
+	if err := session.Query(emailInsertQuery, request.Label, request.Text).Exec(); err != nil {
+		log.Printf("Error inserting data: %v", err)
+		return nil, err
+	}
+
+	return &Routes.EmailResponse{Message: "Email inserted successfully"}, nil
 }
 
 // GetData implements the GetData RPC method
@@ -119,28 +139,45 @@ func (s *server) InsertEducation(ctx context.Context, req *Routes.EducationData)
 func main() {
 	flag.Parse()
 
-	if len(os.Args) != 3 {
-		fmt.Println("Usage: go run main.go <create|delete|grpc> <keyspace_name>")
+	if len(os.Args) > 3 {
+		fmt.Println("Usage: go run main.go <create|delete|grpc> <arg>")
 		os.Exit(1)
 	}
 
 	action := os.Args[1]
-	keyspaceName := os.Args[2]
 
-	dbserver := NewServer()
+	app := NewServer()
 
 	switch action {
 	case "create":
-		err := dbserver.createKeyspace(keyspaceName)
+		keyspaceName := os.Args[2]
+		err := app.createKeyspace(keyspaceName)
 		if err != nil {
 			log.Fatalf("Error creating keyspace: %v", err)
 		}
 	case "delete":
-		err := dbserver.deleteKeyspace(keyspaceName)
+		keyspaceName := os.Args[2]
+		err := app.deleteKeyspace(keyspaceName)
 		if err != nil {
 			log.Fatalf("Error deleting keyspace: %v", err)
 		}
 	case "grpc":
+		if len(os.Args) > 2 {
+			argFlag := os.Args[2]
+			if argFlag != "" {
+				// If arg[2] is not empty, try to convert it to an int and set the port
+				parsedPort, err := strconv.Atoi(argFlag)
+				if err != nil {
+					log.Fatalf("failed to parse port: %v", err)
+				}
+				*port = parsedPort
+			}
+		} else {
+			fmt.Println("Using default port.")
+			// Set your default port here, for example:
+			*port = 50051
+		}
+
 		// Create a TCP listener on port var
 		listener, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))
 		if err != nil {
@@ -149,10 +186,9 @@ func main() {
 
 		// Create a new gRPC server
 		grpcServer := grpc.NewServer()
-		reflection.Register(grpcServer)
 		// Register the DataRoute service implementation with the server
-		Routes.RegisterEducationServiceServer(grpcServer, dbserver)
-		Routes.RegisterEmailServiceServer(grpcServer, dbserver)
+		Routes.RegisterEducationServiceServer(grpcServer, app)
+		Routes.RegisterEmailServiceServer(grpcServer, app)
 
 		// Start the gRPC server as a goroutine
 		go func() {
@@ -162,7 +198,7 @@ func main() {
 			}
 		}()
 
-		// blocks to keep server running
+		// Blocks to keep server running
 		select {}
 	default:
 		fmt.Println("Invalid action. Use 'create', 'delete', or 'grpc'.")
