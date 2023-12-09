0bbb4559448b88510607d99d0f319c200ac89843
Jon Thompson <u1120954@utah.edu>
2023-12-09 18:48:22 +0000

    Removed manual create/delete keyspace. Reimplements the use of -port flag

--- db-manager/Server/server.go
+++ db-manager/Server/server.go
@@ -6,7 +6,7 @@ import (
 	"fmt"
 	"log"
 	"net"
-	"os"
+	"strconv"
 	"strings"
 
 	//"reflect"
@@ -45,60 +45,6 @@ func initDatabaseCluster() *gocql.ClusterConfig {
 	cluster := gocql.NewCluster("scylla") // Add ScyllaDB node IP here
 	cluster.Consistency = gocql.One       // Set the consistency level
 	return cluster
-}
-
-// createKeyspace creates a keyspace in the database.
-func (server *server) createKeyspace(keyspaceName string) error {
-	server.mu.Lock()
-	defer server.mu.Unlock()
-
-	// Create a session to interact with the database (without specifying keyspace)
-	session, err := server.cluster.CreateSession()
-	if err != nil {
-		return err
-	}
-	defer session.Close()
-
-	// Create keyspace query
-	keyspaceQuery := fmt.Sprintf(`
-		CREATE KEYSPACE IF NOT EXISTS %s
-		WITH replication = {
-			'class': 'SimpleStrategy',
-			'replication_factor': 3
-		}
-	`, keyspaceName)
-
-	// Execute keyspace query
-	if err := session.Query(keyspaceQuery).Exec(); err != nil {
-		return err
-	}
-
-	fmt.Printf("Keyspace '%s' created successfully\n", keyspaceName)
-	return nil
-}
-
-// deleteKeyspace deletes a keyspace from the database.
-func (server *server) deleteKeyspace(keyspaceName string) error {
-	server.mu.Lock()
-	defer server.mu.Unlock()
-
-	// Create a session to interact with the database (without specifying keyspace)
-	session, err := server.cluster.CreateSession()
-	if err != nil {
-		return err
-	}
-	defer session.Close()
-
-	// Delete keyspace query
-	keyspaceQuery := fmt.Sprintf("DROP KEYSPACE IF EXISTS %s", keyspaceName)
-
-	// Execute keyspace query
-	if err := session.Query(keyspaceQuery).Exec(); err != nil {
-		return err
-	}
-
-	fmt.Printf("Keyspace '%s' deleted successfully\n", keyspaceName)
-	return nil
 }
 
 // helper method that maps protobuf.Kind and protobuf.Value toCQL data types
@@ -205,56 +151,43 @@ func (s *server) Insert(ctx context.Context, request *Routes.ProtobufInsertReque
 	return &Routes.ProtobufInsertResponse{Errs: []string{}}, nil
 }
 
-// TODO: strip away arg code to just use flags
+// TODO: Create error checking for cmd
 func main() {
 	flag.Parse()
 
-	if len(os.Args) != 3 {
-		fmt.Println("Usage: go run main.go <create|delete|grpc> <keyspace_name>")
-		os.Exit(1)
-	}
-
-	action := os.Args[1]
-	keyspaceName := os.Args[2]
-
 	dbserver := NewServer()
 
-	switch action {
-	case "create":
-		err := dbserver.createKeyspace(keyspaceName)
-		if err != nil {
-			log.Fatalf("Error creating keyspace: %v", err)
-		}
-	case "delete":
-		err := dbserver.deleteKeyspace(keyspaceName)
-		if err != nil {
-			log.Fatalf("Error deleting keyspace: %v", err)
-		}
-	case "grpc":
-		// Create a TCP listener on port var
-		listener, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))
-		if err != nil {
-			log.Fatalf("failed to listen: %v", err)
-		}
-
-		// Create a new gRPC server
-		grpcServer := grpc.NewServer()
-		reflection.Register(grpcServer)
-		// Register the DataRoute service implementation with the server
-		Routes.RegisterDB_InserterServer(grpcServer, dbserver)
-
-		// Start the gRPC server as a goroutine
-		go func() {
-			log.Printf("server listening at %v", listener.Addr())
-			if err := grpcServer.Serve(listener); err != nil {
-				log.Fatalf("failed to serve: %v", err)
+	// Override the port if provided as a command line argument
+	if flag.Parsed() {
+		if portFlag := flag.Lookup("port"); portFlag != nil {
+			portValue, err := strconv.Atoi(portFlag.Value.String())
+			if err != nil {
+				log.Fatalf("failed to parse port: %v", err)
 			}
-		}()
-
-		// blocks to keep server running
-		select {}
-	default:
-		fmt.Println("Invalid action. Use 'create', 'delete', or 'grpc'.")
-		os.Exit(1)
+			port = &portValue
+		}
 	}
+
+	// Create a TCP listener on port var
+	listener, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	// Create a new gRPC server
+	grpcServer := grpc.NewServer()
+	reflection.Register(grpcServer)
+	// Register the DataRoute service implementation with the server
+	Routes.RegisterDB_InserterServer(grpcServer, dbserver)
+
+	// Start the gRPC server as a goroutine
+	go func() {
+		log.Printf("Server listening at %v", listener.Addr())
+		if err := grpcServer.Serve(listener); err != nil {
+			log.Fatalf("failed to serve: %v", err)
+		}
+	}()
+
+	// Blocks to keep the server running
+	select {}
 }
